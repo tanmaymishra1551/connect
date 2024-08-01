1eb74ceadc01a6145f566200a6f98906a5877213
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-08-01 17:52:59 +0530

    Setup files and folder

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react'
+
+const RootaLayout = ({children}: {children: ReactNode})  =>{
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
+
+
+export default RootaLayout
